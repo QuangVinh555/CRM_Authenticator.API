c95a96f87433429ede2dea2e0954fd58009f193b
Võ Quang Vinh <91067804+QuangVinh555@users.noreply.github.com>
2023-12-25 22:23:39 +0700

    Add project files.

---
+++ src/Services/Authenticator/Authenticator.API/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Infrastructure.Entites;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authenticator.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly CRMContext _context;
+
+        public TestController(CRMContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Test() {
+            var test = await _context.AccountModels.Select(x => new
+            {
+                FullName = x.FullName,
+                CreateBy = x.CreateBy,
+            }).ToListAsync();
+            return Ok(test);
+        }
+    }
+}
