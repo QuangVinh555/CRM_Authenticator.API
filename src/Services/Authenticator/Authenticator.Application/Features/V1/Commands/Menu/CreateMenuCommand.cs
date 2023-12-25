4e0dd2f300f512817c4d7f14390ee6b7ec861fbb
Võ Quang Vinh <91067804+QuangVinh555@users.noreply.github.com>
2023-12-25 22:53:57 +0700

    update

--- src/Services/Authenticator/Authenticator.Application/Features/V1/Commands/Menu/CreateMenuCommand.cs
+++ src/Services/Authenticator/Authenticator.Application/Features/V1/Commands/Menu/CreateMenuCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,15 @@ using System.Threading.Tasks;
 
 namespace Authenticator.Application.Features.V1.Commands.Menu
 {
-    public class CreateMenuCommand
+    public class CreateMenuCommand : IRequest<bool>
     {
 
+    }
+    public class CreateMenuCommandHandler : IRequestHandler<CreateMenuCommand, bool>
+    {
+        public Task<bool> Handle(CreateMenuCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
