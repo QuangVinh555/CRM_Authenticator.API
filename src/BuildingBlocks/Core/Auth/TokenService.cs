f0ca427bfad3e76cb94d58784727cfd6173c9985
Võ Quang Vinh <91067804+QuangVinh555@users.noreply.github.com>
2024-01-04 23:19:14 +0700

    update

---
+++ src/BuildingBlocks/Core/Auth/TokenService.cs
@@ -0,0 +1,23 @@
+﻿using Core.Exceptions;
+using Core.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Auth
+{
+    public interface ITokenService
+    {
+        Task<TokenResponse> GetToken(TokenRequest request);
+    }
+    public class TokenService : ITokenService
+    {
+        public Task<TokenResponse> GetToken(TokenRequest request)
+        {
+
+            throw new CommonException(CommonResource.MSG_FAIL, "Đăng nhập");
+        }
+    }
+}
---
+++ src/Services/Authenticator/Authenticator.API/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Core.Auth;
+using Core.Commons;
+using Core.Properties;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authenticator.API.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ITokenService _tokenService;
+
+        public AuthController(ITokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] TokenRequest request)
+        {
+            var res = await _tokenService.GetToken(request);
+            return Ok(new ApiSuccessResponse<TokenResponse>
+            {
+                IsSuccess = true,
+                Message = string.Format(CommonResource.MSG_SUCSSESS, "Đăng nhập"),
+                Data = res
+            }); 
+        }
+    }
+}
