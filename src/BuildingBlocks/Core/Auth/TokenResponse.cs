7b9916d9efc72a41880575e8e0d74e9987818a44
Võ Quang Vinh <91067804+QuangVinh555@users.noreply.github.com>
2024-08-25 21:32:30 +0700

    Revert "update"
    This reverts commit 4f3d33de1c0e8f9a4b5e12d0c3c20ba4520bca14.

--- src/BuildingBlocks/Core/Auth/TokenResponse.cs
+++ src/BuildingBlocks/Core/Auth/TokenResponse.cs
@@ -15,9 +15,6 @@ namespace Core.Auth
         public string RefreshToken { get; set; }
         public DateTime ExpriedTimeAccessToken { get; set; }
         public DateTime ExpriedTimeRefreshToken { get; set; }
-
-        public List<TokenRoleResponse> RoleList { get; set; } = new();
-        public TokenRolePageFunctionResponse AuthenticateList { get; set; } = new();
     }
 
     public class TokenRoleResponse
@@ -29,20 +26,6 @@ namespace Core.Auth
 
     public class TokenRolePageFunctionResponse
     {
-        public string RoleCode { get; set; }
-        public string RoleName { get; set; }
-        public List<PageResponse> PageList { get; set; } = new();
-    }
 
-    public class PageResponse
-    {
-        public Guid? PageId { get; set; }
-        public string PageName { get; set; }
-        public List<FunctionResponse> FunctionList { get; set; } = new();
-    }
-    public class FunctionResponse
-    {
-        public Guid? FunctionId { get; set; }
-        public string FunctionName { get; set; }
     }
 }
