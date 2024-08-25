a9aa2f4554a2f304cf376d3a8267c04f63666e5a
Võ Quang Vinh <91067804+QuangVinh555@users.noreply.github.com>
2024-08-25 21:29:52 +0700

    update git revert

--- src/Services/Authenticator/Authenticator.API/Controllers/MenuController.cs
+++ src/Services/Authenticator/Authenticator.API/Controllers/MenuController.cs
@@ -3,14 +3,12 @@ using Core.Atrributes;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Net.WebSockets;
 
 namespace Authenticator.API.Controllers
 {
     [Route("api/v{version:apiVersion}/[controller]")]
     [ApiVersion("1.0")]
-    //[Authorization]
+    [Authorization]
     [ApiController]
     public class MenuController : ControllerBase
     {
@@ -18,31 +16,7 @@ namespace Authenticator.API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetMenu()
         {
-            List<string> menus = new List<string>{
-                "A", "B", "C", "D","E", "G", "F","H"
-            };
-            List<string> functions = new List<string>{
-                "A", "C", "D","E"
-            };
-            List<string> listRes = new List<string>();
-            List<string> listMessage = new List<string>();
-            foreach (var m in menus) {
-                var a = functions.Where(x => x.Contains(m)).ToList();
-                if(a.Any())
-                {
-                    listRes.AddRange(a);
-                    listMessage.Add("true");
-                }
-                else
-                {
-                    listRes.Add(m);
-                    listMessage.Add("false");
-
-                }
-            }
-            listRes.AddRange(listMessage);
-
-            return Ok(listRes);
+            return Ok(123);
         }
 
         //[HttpPost]
